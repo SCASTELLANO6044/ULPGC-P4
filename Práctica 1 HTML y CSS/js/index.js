a9731dae7bd5b4563c13a4b2b0244dc9faee013f
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-02 21:26:40 +0100

    carousel with no buttons

--- Práctica 1 HTML y CSS/js/index.js
+++ Práctica 1 HTML y CSS/js/index.js
@@ -14,24 +14,5 @@ function showSlides() {
   if (slides[slideIndex - 1]) {
     slides[slideIndex - 1].style.opacity = "1";
   }
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+  setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
-
-
-var prevButton = document.querySelector('.carousel-control.prev');
-var nextButton = document.querySelector('.carousel-control.next');
-prevButton.addEventListener('click', function() {
-  slideIndex--;
-  if (slideIndex < 1) {
-    slideIndex = document.getElementsByClassName("carousel-item").length;
-  }
-  showSlides();
-});
-nextButton.addEventListener('click', function() {
-  slideIndex++;
-  if (slideIndex > document.getElementsByClassName("carousel-item").length) {
-    slideIndex = 1;
-  }
-  showSlides();
-});
-``
