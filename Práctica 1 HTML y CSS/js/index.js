31cf8b641385e3effe46c4a9c36fbdc867f6db92
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-02 20:49:27 +0100

    Automatic image carousel

--- Práctica 1 HTML y CSS/js/index.js
+++ Práctica 1 HTML y CSS/js/index.js
@@ -1,19 +1,37 @@
-var carousel = document.getElementById("carousel");
-var slideIndex = 1;
+var slideIndex = 0;
+showSlides();
 
-function showSlides(n) {
+function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("carousel-img");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("carousel-item");
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.opacity = "0";
   }
-  slides[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slides[slideIndex - 1]) {
+    slides[slideIndex - 1].style.opacity = "1";
+  }
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+var prevButton = document.querySelector('.carousel-control.prev');
+var nextButton = document.querySelector('.carousel-control.next');
+prevButton.addEventListener('click', function() {
+  slideIndex--;
+  if (slideIndex < 1) {
+    slideIndex = document.getElementsByClassName("carousel-item").length;
+  }
+  showSlides();
+});
+nextButton.addEventListener('click', function() {
+  slideIndex++;
+  if (slideIndex > document.getElementsByClassName("carousel-item").length) {
+    slideIndex = 1;
+  }
+  showSlides();
+});
+``
